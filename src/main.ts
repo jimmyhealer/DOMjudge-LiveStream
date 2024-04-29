b245f8293d192425c6cbdc4a0973dfc6636b8d36
jimmyhealer <yahing6066@gmail.com>
2024-04-30 01:57:15 +0800

    feat: add vue-draggable and element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
