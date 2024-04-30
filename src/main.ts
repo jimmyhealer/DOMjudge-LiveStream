b0b8be5979c29550e192c608ec6ad4e7babb722e
jimmyhealer <yahing6066@gmail.com>
2024-04-30 20:20:44 +0800

    feat: add pinia-plugin-persistendstate

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
@@ -11,7 +12,10 @@ import router from './router'
 const app = createApp(App)
 
 app.use(ElementPlus)
-app.use(createPinia())
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
