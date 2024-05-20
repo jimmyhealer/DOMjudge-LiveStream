f03c7e473eb6c0ca9e5f3bc29f6819df71b0b390
jimmyhealer <yahing6066@gmail.com>
2024-05-20 23:35:41 +0800

    feat: update submission item style

--- src/components/SubmissionItem.vue
+++ src/components/SubmissionItem.vue
@@ -4,11 +4,45 @@ import type { LastSubmission } from '@/types'
 defineProps<{
   data: LastSubmission
 }>()
+
+function hexToRgb(hex: string) {
+  const bigint = parseInt(hex.slice(1), 16)
+  const r = (bigint >> 16) & 255
+  const g = (bigint >> 8) & 255
+  const b = bigint & 255
+  return [r, g, b]
+}
+
+function adjustTextColor(color: string) {
+  const rgb = hexToRgb(color)
+  // const brightness = (0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0772 * rgb[2]) / 255
+  const brightness = (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 255
+  return brightness >= 0.5 ? 'black' : 'white'
+}
+
+function getRankColor(rank: number) {
+  if (rank === 1) {
+    return '#FFD700'
+  } else if (rank === 2) {
+    return '#C0C0C0'
+  } else if (rank === 3) {
+    return '#CD7F32'
+  } else {
+    return '#777'
+  }
+}
 </script>
 <template>
   <el-row>
     <el-col :span="2">
-      <span style="background-color: #777; color: #fff"> {{ data.rank }} </span>
+      <span
+        :style="{
+          'background-color': getRankColor(data.rank),
+          color: adjustTextColor(getRankColor(data.rank))
+        }"
+      >
+        {{ data.rank }}
+      </span>
     </el-col>
     <el-col :span="14">
       <span
@@ -30,7 +64,8 @@ defineProps<{
     <el-col :span="2">
       <span
         :style="{
-          'border-bottom': '3px solid ' + data.problem.color
+          'background-color': data.problem.color,
+          color: adjustTextColor(data.problem.color)
         }"
       >
         {{ data.problem.shortName }}
