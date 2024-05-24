0d76c157fc8435d4c68b97e1b770fd031b49a0a8
jimmyhealer <yahing6066@gmail.com>
2024-05-24 22:49:34 +0800

    update scorebord height 64px

--- src/views/LiveStream.vue
+++ src/views/LiveStream.vue
@@ -36,7 +36,7 @@ onMounted(async () => {
             <template v-if="checkScoreboard(id)">
               <div
                 :style="{
-                  height: `calc(${100 / contestStore.contests!.length}% + ${idx ? '-48' : '+48'}px)`
+                  height: `calc(${100 / contestStore.contests!.length}% + ${idx ? '-64' : '+64'}px)`
                 }"
                 style="position: relative"
               >
